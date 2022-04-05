85808d4daedddbc44da178757530af076021749c
grigoar <grigore.nath.j@gmail.com>
2022-04-05 06:21:45 +0300

    finished section 27: React + Typescript

--- 22.react-typescript/react-ts/src/components/TodoItem.tsx
+++ 22.react-typescript/react-ts/src/components/TodoItem.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
 import classes from './TodoItem.module.css';
 // const Item: React.FC<{ id: string; text: string }> = (props) => {
-const Item: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const Item: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
+  return (
+    <li onClick={props.onRemoveTodo} className={classes.item}>
+      {props.text}
+    </li>
+  );
 };
 
 export default Item;
--- 22.react-typescript/react-ts/src/components/Todos.tsx
+++ 22.react-typescript/react-ts/src/components/Todos.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import Todo from '../models/todo';
+import React, { useContext } from 'react';
+// import Todo from '../models/todo';
 import TodoItem from './TodoItem';
 import classes from './Todos.module.css';
+import { TodosContext } from '../store/todos-context';
 
 // const Todos: React.FC<{ items: string[] }> = (props) => {
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+// const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+//   props
+// ) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem text={item.text} key={item.id}></TodoItem>
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+          text={item.text}
+          key={item.id}
+        ></TodoItem>
         // <li key={item.id}>{item.text}</li>
       ))}
     </ul>
@@ -16,3 +25,18 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
 };
 
 export default Todos;
+//   return (
+//     <ul className={classes.todos}>
+//       {props.items.map((item) => (
+//         <TodoItem
+//           onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+//           text={item.text}
+//           key={item.id}
+//         ></TodoItem>
+//         // <li key={item.id}>{item.text}</li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default Todos;
